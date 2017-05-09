913f775ff22a38e178fbf3a231fadf30385f0dd0
Kevin Zhao <kvn.zhl@gmail.com>
2017-05-09 19:30:30 +0800

    Update index.js

--- functions/ResizeImage/index.js
+++ functions/ResizeImage/index.js
@@ -2,30 +2,32 @@ var Jimp = require("jimp");
 
 module.exports = (context) => {
 
-    context.log(`Starting...`);
+  context.log(`Starting...`);
 
-    // Read image with Jimp
-    Jimp.read(context.bindings.inputBlob).then((image) => {
+  // Read image with Jimp
+  Jimp.read(context.bindings.inputBlob).then((image) => {
 
-        context.log(`Processing...`);
+    context.log(`Processing...`);
 
-        // Manipulate image
-        image
-            .resize(100, Jimp.AUTO) 
-            .getBuffer(Jimp.MIME_JPEG, (error, stream) => {
-                // Check for errors
-                if (error) {
-                    context.log(`There was an error processing the image.`);
-                    context.done(error);
-                } else {
-                    context.log('Node.JS blob trigger function resized ' + context.bindingData.name + ' to ' + image.bitmap.width + 'x' + image.bitmap.height);
-                    context.bindings.outputBlob = stream;
-                    context.done();
-                }
-            });
+    // Manipulate image
+    image
+      .resize(60, Jimp.AUTO)
+      .getBuffer(Jimp.MIME_JPEG, (error, stream) => {
+        // Check for errors
+        if (error) {
+          // TODO: Persist the failure for further processing
+          context.log(`There was an error processing the image.`);
+          context.done(error);
+        } else {
+          context.log('Node.JS blob trigger function resized ' + context.bindingData.name + ' to ' + image.bitmap.width + 'x' + image.bitmap.height);
+          context.bindings.outputBlob = stream;
+          context.done();
+        }
+      });
 
-    }).catch(function (err) {
-        context.log(err);
-    });
+  }).catch(function (err) {
+    // TODO: Persist the failure for further processing
+    context.log(err);
+  });
 
 };
